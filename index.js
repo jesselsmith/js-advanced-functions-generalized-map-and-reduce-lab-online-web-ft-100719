301bce341eb64ca1ffe0a01303c266e6022ad01d
Jesse <jesse.lee.smith@gmail.com>
2020-02-06 14:37:21 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+function map(sourceArray, callbackFunction) {
+  const newArray = []
+  sourceArray.forEach(element => newArray.push(callbackFunction(element)))
+  return newArray
+}
+
+[1, 2, 3].reduce()
+
+function reduce(sourceArray, callbackFunction, startingPoint) {
+  let memo = startingPoint
+  let workingArray = []
+  if (memo === undefined) {
+    memo = sourceArray[0]
+    workingArray = sourceArray.slice(1)
+  } else {
+    workingArray = sourceArray
+  }
+  workingArray.forEach(element => memo = callbackFunction(memo, element))
+  return memo
+}
